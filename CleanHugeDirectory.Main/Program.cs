b0a41e6b905e4c5df173d75272ff485bcfb203b1
Shimshon Fishler <shimshon.fishler@gmail.com>
2016-02-09 07:06:43 +0200

    Delete from the proper directory!!

--- CleanHugeDirectory.Main/Program.cs
+++ CleanHugeDirectory.Main/Program.cs
@@ -15,19 +15,28 @@ namespace CleanHugeDirectory.Main
                 for(char A2 = 'A'; A2 <= 'Z'; A2++)
                 {
                     var prefix = string.Format("{0}{1}*.HTM", A1, A2);
-                    Console.WriteLine("Prefix:{0}", prefix);
-                    foreach (var file in GetFilesUnmanaged(@"C:\Users\Administrator\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5", prefix))
-                        File.Delete(file);
+                    DeleteFilesByPrefix(@"C:\Users\Administrator\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5", prefix);
                 }
 
             for (char A1 = '0'; A1 <= '9'; A1++)
                 for (char A2 = '0'; A2 <= '9'; A2++)
                 {
                     var prefix = string.Format("{0}{1}*.HTM", A1, A2);
-                    Console.WriteLine("Prefix:{0}", prefix);
-                    foreach (var file in GetFilesUnmanaged(@"C:\Users\Administrator\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5", prefix))
-                        File.Delete(file);
+                    DeleteFilesByPrefix(@"C:\Users\Administrator\AppData\Local\Microsoft\Windows\Temporary Internet Files\Content.IE5", prefix);
                 }
+        }
+
+        public static void DeleteFilesByPrefix(string path, string prefix)
+        {
+            Console.WriteLine("Prefix:{0}", prefix);
+            var i = 0;
+            foreach (var file in GetFilesUnmanaged(path, prefix))
+            {
+                //Console.WriteLine(file);
+                File.Delete(Path.Combine(path, file));
+                i++;
+            }
+            Console.WriteLine("{0} files deleted", i);
         }
 
         public static IEnumerable<string> GetFilesUnmanaged(string directory, string filter)
